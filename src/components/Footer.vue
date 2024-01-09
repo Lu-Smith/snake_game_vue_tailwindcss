d9b0cce2acb30904f5f516ca8a1e6df8bcc85b06
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-01-09 22:40:16 +0000

    Updated content of Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,3 +1,20 @@
 <template>
-    Footer
+    <h3 class="fixed bottom-4 right-4 w-ful text-sm mt-2 bg-bgColor 
+    p-2 rounded text-textColor font-semibold">
+        Coded by 
+        <a 
+        href="https://www.lunasmithart.com/"
+        target="_blank"
+        class="text-linksColor border-b-2 border-linksColor 
+        hover:border-none hover:text-bgColor transition-all duration-300 ease-in-out 
+        hover:bg-addColor underline-offset-4 hover:underline-offset-8 rounded hover:p-2">
+        Luna Smith</a> -
+        <a 
+        href="https://github.com/Lu-Smith/snake_game_vue_tailwindcss" 
+        target="_blank"
+        class="text-linksColor border-b-2 border-linksColor 
+        hover:border-none hover:text-bgColor transition-all duration-300 ease-in-out 
+        hover:bg-addColor underline-offset-4 hover:underline-offset-8 rounded hover:p-2">
+        GitHub</a>.
+    </h3>
 </template>
