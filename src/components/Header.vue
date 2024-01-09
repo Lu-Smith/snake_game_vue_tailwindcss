8edd1ffdadd34ce333049c5ea6a29d5743bfee1e
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-01-09 22:37:20 +0000

    Updated template in Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,6 @@
 <template>
-    Header
+    <div>
+        <h1>Welcome to Snake Game! </h1>
+    <p>Enjoy the classic thrill of maneuvering a snake through the digital realm. Have fun and challenge yourself to grow as long as possible. Best of luck!</p>
+    </div>
 </template>
