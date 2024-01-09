c74d47f4b7fafa626dbdc0b697a2618454d7c9c6
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-01-09 22:29:21 +0000

    Updated content of App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,9 @@
 </script>
 
 <template>
-  <div>
-    Hello World
+  <div class="bg-red-200">
+    <h1>Welcome to Snake Game! </h1>
+    <p>Enjoy the classic thrill of maneuvering a snake through the digital realm. Have fun and challenge yourself to grow as long as possible. Best of luck!</p>
   </div>
 </template>
 
