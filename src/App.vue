88711b4d0224b24226c45f9f2aa4a3cbabe14a7c
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-01-09 22:39:01 +0000

    Added basic components to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
+import Header from './components/Header.vue';
+import Game from './components/Game.vue';
+import Footer from './components/Footer.vue';
+
 </script>
 
 <template>
   <div class="bg-red-200">
-    <h1>Welcome to Snake Game! </h1>
-    <p>Enjoy the classic thrill of maneuvering a snake through the digital realm. Have fun and challenge yourself to grow as long as possible. Best of luck!</p>
+    <Header />
+    <Game />
+    <Footer />
   </div>
 </template>
 
