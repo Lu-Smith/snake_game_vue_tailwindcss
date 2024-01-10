726d1165a4d38c2bc07f48b880616be267a8aced
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-01-10 15:03:51 +0000

    Added canvas to Game.vue

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -3,14 +3,28 @@
         <button :class="mode ? 'text-textColor' : 'text-addColor'">Start</button>
         <button :class="mode ? 'text-textColor' : 'text-addColor'">Restart</button>
     </div>
-    <div :class="mode ? 'text-textColor' : 'text-addColor'">
-        <div class="grid grid-rows-4 grid-flow-col gap-4">
-            <div v-for="block in blocks" :key="block">😄</div>
-        </div>
+    <div 
+    
+    class="w-full h-full mt-4">
+        <canvas :class="mode ? 'bg-green-200' : 'bg-red-200'"  ref="gameCanvas" :width="canvasWidth" :height="canvasHeight"></canvas>
     </div> 
 </template>
 
 <script lang="ts" setup>
+import { ref, onMounted } from 'vue';
+
 defineProps(['mode']);
- const blocks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 ,15, 16]
+
+const canvasWidth = '80%';
+const canvasHeight = '80%';
+
+onMounted(() => {
+const canvas = ref<HTMLCanvasElement | null>(null);
+const context = canvas.value?.getContext('2d');
+
+if (context) {
+    context.fillStyle = 'green';
+    context.fillRect(10, 10, 50, 50);
+  }
+});
 </script>
