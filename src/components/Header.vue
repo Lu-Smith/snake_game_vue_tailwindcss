99393b9a14d6344937ad5ed7c8a901ab0978ddde
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-01-10 13:11:09 +0000

    Added icons to Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,13 @@
 <template>
     <div>
-        <div>Mood</div>
+        <div>
+            <span class="material-symbols-outlined">
+            emoji_objects
+            </span>
+            <span class="material-symbols-outlined">
+            nightlight
+            </span>
+        </div>
         <h1>Welcome to Snake Game! </h1>
         <p>Enjoy the classic thrill of maneuvering a snake through the digital realm. Have fun and challenge yourself to grow as long as possible. Best of luck!</p>
     </div>
