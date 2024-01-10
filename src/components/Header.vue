59601cb9ab63a283cc72d06534c55dcc05d8c8a7
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-01-10 13:13:03 +0000

    Added switch icons to Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,22 @@
 <template>
     <div>
         <div>
-            <span class="material-symbols-outlined">
-            emoji_objects
-            </span>
-            <span class="material-symbols-outlined">
-            nightlight
-            </span>
+            <div>
+                <span class="material-symbols-outlined">
+                emoji_objects
+                </span>
+                <span class="material-symbols-outlined">
+                toggle_off
+                </span>
+            </div>
+            <div>
+                <span class="material-symbols-outlined">
+                toggle_on
+                </span>
+                <span class="material-symbols-outlined">
+                nightlight
+                </span>
+            </div>
         </div>
         <h1>Welcome to Snake Game! </h1>
         <p>Enjoy the classic thrill of maneuvering a snake through the digital realm. Have fun and challenge yourself to grow as long as possible. Best of luck!</p>
