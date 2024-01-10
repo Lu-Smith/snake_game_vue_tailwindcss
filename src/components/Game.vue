71f2fe5eec6e371bdf4b7dbedd7a40029cef27fd
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-01-10 14:34:05 +0000

    Updated button in Game

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,7 +1,9 @@
 <template>
-    <button :class="mode ? 'text-textColor' : 'text-addColor'">Start</button>
-    <div :class="mode ? 'text-textColor' : 'text-addColor'">Start</div>
-
+    <div class="flex flex-row justify-center items-center gap-6">
+        <button :class="mode ? 'text-textColor' : 'text-addColor'">Start</button>
+        <button :class="mode ? 'text-textColor' : 'text-addColor'">Restart</button>
+    </div>
+    <div :class="mode ? 'text-textColor' : 'text-addColor'">Start</div> 
 </template>
 
 <script lang="ts" setup>
