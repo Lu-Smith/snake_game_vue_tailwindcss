4c79a757635bdc77e6ce3752d0f58a40315c5f2f
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-01-10 13:58:13 +0000

    Updated logic of the Switch

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,6 @@
 <template>
   <div class="flex flex-col justify-center items-center gap-6 w-full h-full">
-    <div class="text-center">
-            <div 
-            @click="darkMode"
-            v-if="mode"
-            class="text-green-600" >
-                <span class="material-symbols-outlined text-lightColor text-4xl">
-                emoji_objects
-                </span>
-                <span class="material-symbols-outlined text-4xl">
-                toggle_off
-                </span>
-            </div>
-            <div 
-            @click="lightMode"
-            v-else
-            class="text-red-600">
-                <span class="material-symbols-outlined text-darkColor text-4xl">
-                nightlight
-                </span>
-                <span class="material-symbols-outlined text-4xl">
-                toggle_on
-                </span>
-            </div>
-        </div>
+    <Switch v-model:mode="mode" />
     <div class="bg-green-200 w-full h-full p-6">
       <Header />
     </div>
@@ -39,15 +16,8 @@ import { ref } from 'vue';
 import Header from './components/Header.vue';
 import Game from './components/Game.vue';
 import Footer from './components/Footer.vue';
+import Switch from './components/Switch.vue';
 
-const mode =ref(false);
-
-function lightMode() {
-    mode.value = !mode.value
-};
-
-function darkMode() {
-    mode.value = !mode.value
-};
+const mode = ref(false);
 </script>
 
--- src/components/Switch.vue
+++ src/components/Switch.vue
@@ -1,26 +1,21 @@
 <template>
     <div class="text-center">
-        <div 
-        @click="darkMode"
-        v-if="mode"
-        class="text-green-600" >
-            <span class="material-symbols-outlined text-lightColor text-4xl">
-            emoji_objects
-            </span>
-            <span class="material-symbols-outlined text-4xl">
-            toggle_off
-            </span>
-        </div>
-        <div 
-        @click="lightMode"
-        v-else
-        class="text-red-600">
-            <span class="material-symbols-outlined text-darkColor text-4xl">
-            nightlight
-            </span>
-            <span class="material-symbols-outlined text-4xl">
-            toggle_on
-            </span>
-        </div>
+      <div @click="toggleMode" :class="mode ? 'text-green-600' : 'text-red-600'">
+        <span v-if="mode" class="material-symbols-outlined text-lightColor text-4xl">emoji_objects</span>
+        <span v-else class="material-symbols-outlined text-darkColor text-4xl">nightlight</span>
+        <span class="material-symbols-outlined text-4xl">toggle_{{ mode ? 'off' : 'on' }}</span>
+      </div>
     </div>
-</template>
+  </template>
+  
+  <script lang="ts" setup>
+  import { defineProps, defineEmits } from 'vue';
+  
+  const props = defineProps(['mode']);
+  const emits = defineEmits();
+  
+  const toggleMode = () => {
+    emits('update:mode', !props.mode);
+    console.log(props.mode)
+  };
+  </script>
