6b119c0d8e088b2e1a6fc45b0ce342109cf7ce04
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-01-10 14:04:10 +0000

    Updated Header modes

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,10 @@
 <template>
-    <div>
+    <div :class="mode ? 'text-textColor' : 'text-addColor'">
         <h1>Welcome to Snake Game! </h1>
         <p>Enjoy the classic thrill of maneuvering a snake through the digital realm. Have fun and challenge yourself to grow as long as possible. Best of luck!</p>
     </div>
 </template>
+
+<script lang="ts" setup>
+defineProps(['mode']);
+</script>
