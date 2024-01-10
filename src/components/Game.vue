5471be8f978ee7da56d16d84ce350d5869edbf9e
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-01-10 14:39:08 +0000

    Updated logic in Game.vue

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -3,9 +3,14 @@
         <button :class="mode ? 'text-textColor' : 'text-addColor'">Start</button>
         <button :class="mode ? 'text-textColor' : 'text-addColor'">Restart</button>
     </div>
-    <div :class="mode ? 'text-textColor' : 'text-addColor'">Start</div> 
+    <div :class="mode ? 'text-textColor' : 'text-addColor'">
+        <div class="grid grid-rows-4 grid-flow-col gap-4">
+            <div v-for="block in blocks" :key="block">😄</div>
+        </div>
+    </div> 
 </template>
 
 <script lang="ts" setup>
 defineProps(['mode']);
+ const blocks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 ,15, 16]
 </script>
