19e2e4e4c054bfafabcf0e6f9d35f3413947a6af
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-01-12 12:00:58 +0000

    Updated Game.vue

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,16 +1,16 @@
 <template>
     <GameControls :mode="mode" @startGame="startGame"/>
     <div class="w-full h-full mt-4 bg-red-200 flex justify-center items-center">
-      <canvas
-        :class="mode ? 'bg-green-200' : 'bg-slate-800'"
-        ref="gameCanvas"
-        :style="{ height: canvasHeight, width: canvasWidth }"
-      ></canvas>
-    </div>
+    <canvas
+      :class="mode ? 'bg-green-200' : 'bg-slate-800'"
+      ref="gameCanvas"
+      :style="{ height: canvasHeight, width: canvasWidth }"
+    ></canvas>
+  </div>
   </template>
   
   <script lang="ts" setup>
-    import { ref, onMounted, toRefs } from 'vue';
+    import { ref, onMounted, toRefs} from 'vue';
     import Apple from '../assets/smallApple.png';
     import GameControls from './Game/GameControls.vue';
 
