1212cb298d6a7a2f0e054fad86881e11443dedb7
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-01-12 15:34:16 +0000

    Updated Score.vue

--- src/components/Game/Score.vue
+++ src/components/Game/Score.vue
@@ -1,12 +1,13 @@
 <template>
     <div class="flex flex-row justify-center items-center bg-slate-400 p-2">
         <h3 class="font-semibold text-2xl underline">You ate 
-            <span class="text-4xl text-linksColor">0</span>
+            <span class="text-4xl text-linksColor">{{ score }}</span>
         </h3>
         <img src='../../assets/smallApple.png' alt="apple" class="h-[40px] w-[40px]">
     </div>
 </template>
 
 <script lang="ts" setup>
+defineProps(['score'])
     
 </script>
