b5c8e00c6dd8226956aa8296cb3236865c667e15
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-01-12 15:33:27 +0000

    Updated Score.vue

--- src/components/Game/Score.vue
+++ src/components/Game/Score.vue
@@ -1,4 +1,12 @@
 <template>
-<h3>You ate</h3>
-<img src="" alt="">
+    <div class="flex flex-row justify-center items-center bg-slate-400 p-2">
+        <h3 class="font-semibold text-2xl underline">You ate 
+            <span class="text-4xl text-linksColor">0</span>
+        </h3>
+        <img src='../../assets/smallApple.png' alt="apple" class="h-[40px] w-[40px]">
+    </div>
 </template>
+
+<script lang="ts" setup>
+    
+</script>
