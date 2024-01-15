64af7bb271233b097c0582f2f34143bb437b66c2
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-01-15 22:44:27 +0000

    Updated Header styling

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,10 @@
 <template>
     <div 
     class="text-center"
-    :class="mode ? 'text-textColor' : 'text-addColor'">
+    :class="mode ? 'text-addColor' : 'text-textColor'">
         <h1 class="text-xl md:text-2xl font-semibold mb-4">Welcome to Snake Game! </h1>
-        <p>Enjoy the classic thrill of maneuvering a snake through the digital realm. Have fun and challenge yourself to grow as long as possible. Best of luck!</p>
+        <p class="text-xs sm:text-sm md:text-base">Enjoy the classic thrill of maneuvering a snake through the digital realm. 
+            Have fun and challenge yourself to grow as long as possible. Best of luck!</p>
     </div>
 </template>
 
