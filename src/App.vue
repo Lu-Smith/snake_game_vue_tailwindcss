961dd254854931f293829dfa54ec6e730526e476
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-01-15 22:47:51 +0000

    Updated styling in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="flex flex-col justify-center items-center gap-6 w-full h-full">
+  <div 
+  :class="mode ? 'bg-linksColor' : 'bg-bgColor'"
+  class="flex flex-col justify-center items-center gap-6 w-full h-full">
     <Switch v-model:mode="mode" />
-    <div class="bg-green-200 w-full h-full p-6">
+    <div class="w-full h-full p-6">
       <Header :mode="mode"  />
     </div>
     <div class="bg-blue-200 w-full h-full p-6">
