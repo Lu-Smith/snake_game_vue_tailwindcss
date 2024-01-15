e7a2dc9b607c6fc8358c93c7127fc16b07367caf
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-01-15 23:08:06 +0000

    Updated styling in GameControls.vue

--- src/components/Game/GameControls.vue
+++ src/components/Game/GameControls.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="flex flex-row justify-center items-center gap-6">
-    <button @click="handleStartGame" :class="mode ? 'text-textColor' : 'text-addColor'">Start</button>
-    <button @click="handleStartGame" :class="mode ? 'text-textColor' : 'text-addColor'">Restart</button>
+    <button 
+    @click="handleStartGame"
+    class="px-4 py-2 font-semibold rounded-lg" 
+    :class="mode ? 'bg-red-400 text-bgColor' : 'bg-red-600 text-textColor'"
+    >Start</button>
+    <button 
+    class="px-4 py-2 text-bgColor font-semibold rounded-lg"
+    @click="handleStartGame" 
+    :class="mode ? 'bg-red-400 text-bgColor' : 'bg-red-600 text-textColor'"
+    >Restart</button>
   </div>
 </template>
 
