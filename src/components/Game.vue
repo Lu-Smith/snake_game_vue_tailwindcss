d8404553cb0107fb1875eeb48ff48c6c12624ec5
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-01-16 19:50:28 +0000

    Updated styling  in Game.vue

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -2,7 +2,7 @@
     <GameControls :mode="mode" @startGame="startGame" />
     <div class="w-full h-full mt-4 flex justify-center items-center">
         <canvas
-        :class="mode ? 'bg-green-200' : 'bg-slate-800'"
+        :class="mode ? 'bg-red-100' : 'bg-slate-800'"
         ref="gameCanvas"
         :style="{ height: canvasHeight, width: canvasWidth }"
         ></canvas>
