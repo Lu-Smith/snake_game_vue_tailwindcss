a7c49c68bce17c0ae45fa83713b80c717dff6902
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-01-16 19:47:12 +0000

    Updated Score.vue

--- src/components/Game/Score.vue
+++ src/components/Game/Score.vue
@@ -1,13 +1,16 @@
 <template>
-    <div class="flex flex-row justify-center items-center bg-slate-400 p-2">
-        <h3 class="font-semibold text-2xl underline">You ate 
-            <span class="text-4xl text-linksColor">{{ score }}</span>
-        </h3>
-        <img src='../../assets/smallApple.png' alt="apple" class="h-[40px] w-[40px]">
+    <div class="flex flex-row justify-center items-center p-2">
+        <h3 
+        :class="mode ? 'text-bgColor' : 'text-textColor'"
+        class="font-semibold text-2xl">You ate</h3>
+        <div class="flex flex-row justify-center items-center p-2">
+            <span class="text-4xl text-red-600">{{ score }}</span>
+            <img src='../../assets/smallApple.png' alt="apple" class="h-[40px] w-[40px]">
+        </div>
     </div>
 </template>
 
 <script lang="ts" setup>
-defineProps(['score'])
+defineProps(['score', 'mode'])
     
 </script>
