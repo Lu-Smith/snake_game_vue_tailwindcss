b378babb03daa172a7bf126277dee6357d6332bd
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-01-17 19:54:56 +0000

    Simplified Score.vue

--- src/components/Game/Score.vue
+++ src/components/Game/Score.vue
@@ -1,30 +1,15 @@
 <template>
-    <div 
-    v-if="gameRunning"
-    class="flex flex-row justify-center items-center p-2">
-        <h3 
-        :class="mode ? 'text-bgColor' : 'text-textColor'"
-        class="font-semibold text-2xl">You ate</h3>
-        <div class="flex flex-row justify-center items-center p-2">
-            <span class="text-4xl text-red-600">{{ score }}</span>
-            <img src='../../assets/smallApple.png' alt="apple" class="h-[40px] w-[40px]">
-        </div>
+    <div v-if="gameRunning || (score !== 0 && !gameRunning)" class="flex flex-row justify-center items-center p-2">
+      <h3 :class="mode ? 'text-bgColor' : 'text-textColor'" class="font-semibold text-xl">
+        You {{ gameRunning ? 'ate' : 'lost, try again... You ate' }}
+      </h3>
+      <div class="flex flex-row justify-center items-center p-2">
+        <span class="text-red-600" :class="gameRunning ? 'text-4xl' : 'text-2xl'">{{ score }}</span>
+        <img src="../../assets/smallApple.png" alt="apple" :class="gameRunning ? 'h-[40px] w-[40px]' : 'h-[30px] w-[30px]'">
+      </div>
+      <h3 v-if="!gameRunning" :class="mode ? 'text-bgColor' : 'text-textColor'" class="font-semibold text-base">.</h3>
     </div>
-    <div 
-    v-if="score !== 0 && !gameRunning"
-    class="flex flex-row justify-center items-center p-2">
-        <h3 
-        :class="mode ? 'text-bgColor' : 'text-textColor'"
-        class="font-semibold text-base">You lost, try again... You ate</h3>
-        <div class="flex flex-row justify-center items-center p-2 pr-0">
-            <span class="text-2xl text-red-600">{{ score }}</span>
-            <img src='../../assets/smallApple.png' alt="apple" class="h-[30px] w-[30px]">
-        </div>
-        <h3 
-        :class="mode ? 'text-bgColor' : 'text-textColor'"
-        class="font-semibold text-base">.</h3>
-    </div>
-</template>
+  </template>
 
 <script lang="ts" setup>
 defineProps(['score', 'mode', 'gameRunning', 'initialSnake', 'snake'])
