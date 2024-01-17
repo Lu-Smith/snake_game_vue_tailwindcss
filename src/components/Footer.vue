c4c93d8e96f849a42164c0fff89de52e4d446e2f
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-01-17 20:05:29 +0000

    Updated Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,21 +1,22 @@
 <template>
     <h3 
-    :class="mode ? 'text-textColor' : 'text-addColor'"
-    class="fixed bottom-4 right-4 w-ful text-sm mt-2 bg-bgColor 
-    p-2 rounded font-semibold">
+    :class="mode ? 'text-addColor border-linksColor bg-red-200' : 'text-textColor border-addColor bg-bgColor'"
+    class="text-sm   p-2 font-semibold text-center border-t-2 ">
         Coded by 
         <a 
         href="https://www.lunasmithart.com/"
         target="_blank"
-        class="text-linksColor border-b-2 border-linksColor 
-        hover:border-none hover:text-bgColor transition-all duration-300 ease-in-out 
+        :class="mode ? 'border-addColor' : 'border-linksColor'"
+        class="text-textColor border-b-2
+        hover:border-none  transition-all duration-300 ease-in-out 
         hover:bg-addColor underline-offset-4 hover:underline-offset-8 rounded hover:p-2">
         Luna Smith</a> -
         <a 
         href="https://github.com/Lu-Smith/snake_game_vue_tailwindcss" 
         target="_blank"
-        class="text-linksColor border-b-2 border-linksColor 
-        hover:border-none hover:text-bgColor transition-all duration-300 ease-in-out 
+        :class="mode ? 'border-addColor' : 'border-linksColor'"
+        class="text-textColor border-b-2 
+        hover:border-none  transition-all duration-300 ease-in-out 
         hover:bg-addColor underline-offset-4 hover:underline-offset-8 rounded hover:p-2">
         GitHub</a>.
     </h3>
