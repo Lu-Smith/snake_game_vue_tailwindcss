3af1cbe710d16a68891c2f0707630171dbb80f93
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-01-17 20:29:49 +0000

    Updated GameControls.vue

--- src/components/Game/GameControls.vue
+++ src/components/Game/GameControls.vue
@@ -7,18 +7,21 @@
     :class="mode ? 'bg-red-400 text-textColor hover:bg-red-200' : 'bg-red-600 text-textColor hover:bg-red-400'"
     >Start</button>
     <button 
-    @click="handleStartGame" 
+    @click="handlePauseGame" 
     v-else
     class="px-4 py-2 font-semibold rounded-lg shadow-sm text-sm md:text-base transition-all duration-300 ease-in-out"
     :class="mode ? 'bg-addColor text-textColor hover:bg-linksColor' : 'bg-bgColor text-textColor hover:bg-addColor'"
-    >Restart</button>
+    >Pause</button>
   </div>
 </template>
 
 <script lang="ts" setup>
 defineProps(['mode', 'gameRunning']);
-const emits = defineEmits(['startGame']);
+const emits = defineEmits(['startGame', 'pauseGame']);
 const handleStartGame = () => {
   emits('startGame');
+};
+const handlePauseGame = () => {
+  emits('pauseGame');
 };
 </script>
