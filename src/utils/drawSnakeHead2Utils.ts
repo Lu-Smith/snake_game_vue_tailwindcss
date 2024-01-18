6d2acbc8daebc167e639035e85495aa4d5d83589
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-01-18 18:07:53 +0000

    Created second snake

---
+++ src/utils/drawSnakeHead2Utils.ts
@@ -0,0 +1,60 @@
+export function drawSnakeHead(
+    context: CanvasRenderingContext2D,
+    x: number,
+    y: number,
+    radius: number,
+    direction: number[],
+  ) {
+  // Head
+  context.beginPath();
+  context.arc(x, y, radius, 0, 2 * Math.PI, false);
+  context.fillStyle = "#8ef6e4";
+  context.fill();
+  context.lineWidth = 1;
+  context.strokeStyle = '#8ef6e4';
+  context.stroke();
+
+  // Eyes
+  let eyeOffsetrX = 0;
+  let eyeOffsetrY = 0;
+  let eyeOffsetlX = 0;
+  let eyeOffsetlY = 0;
+
+  if (direction[0] === 1) {
+    // Moving right
+    eyeOffsetlX = 1.5;
+    eyeOffsetrX = 1.5;
+    eyeOffsetlY = 2;
+    eyeOffsetrY = 2.5;
+  } else if (direction[0] === -1) {
+    // Moving left
+    eyeOffsetlX = -1.5;
+    eyeOffsetrX = -1.5;
+    eyeOffsetlY = 2;
+    eyeOffsetrY = 2.5;
+  }
+
+  if (direction[1] === 1) {
+    // Moving down
+    eyeOffsetlX = 2;
+    eyeOffsetrX = -2;
+    eyeOffsetlY = 2.5;
+    eyeOffsetrY = -2.5;
+  } else if (direction[1] === -1) {
+    // Moving up
+    eyeOffsetlX = 2;
+    eyeOffsetrX = -2;
+    eyeOffsetlY = -2;
+    eyeOffsetrY = 2;
+  }
+
+  context.beginPath();
+  context.arc(x + eyeOffsetlX, y + eyeOffsetlY, 1, 0, 2 * Math.PI, false); // Left eye
+  context.fillStyle = "#000";
+  context.fill();
+
+  context.beginPath();
+  context.arc(x + eyeOffsetrX, y - eyeOffsetrY, 1, 0, 2 * Math.PI, false); // Right eye
+  context.fillStyle = "#000";
+  context.fill();
+  }
