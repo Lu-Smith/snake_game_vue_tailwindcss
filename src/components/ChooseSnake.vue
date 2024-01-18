d80b6e5f30b4f82fcc15817e00550a2c2ec4ff20
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-01-18 18:26:34 +0000

    Updated ChooseSnake.vue

--- src/components/ChooseSnake.vue
+++ src/components/ChooseSnake.vue
@@ -1,16 +1,19 @@
 <template>
-    <div class="w-full h-full mt-4 flex justify-center items-center">
-        <canvas
-        :class="mode ? 'bg-red-100' : 'bg-slate-800'"
-        class="border-2 border-focusColor"
-        ref="gameCanvas"
-        ></canvas>
+    <div class="flex justify-center items-center flex-col gap-4">
+        <div class="w-full h-full mt-4 flex justify-center items-center">
+            <canvas
+            :class="mode ? 'bg-red-100' : 'bg-slate-800'"
+            class="border-2 border-focusColor"
+            ref="gameCanvas"
+            ></canvas>
+        </div>
+        <button
+        class="px-4 py-2 font-semibold rounded-lg shadow-sm text-sm md:text-base 
+        transition-all duration-300 ease-in-out border-2" 
+        :class="mode ? 'bg-red-400 text-textColor hover:bg-red-200 border-focusColor' 
+        : 'bg-bgColor text-textColor hover:bg-red-400 border-focusColor'">My Snake</button>
     </div>
-    <button
-    class="px-4 py-2 font-semibold rounded-lg shadow-sm text-sm md:text-base 
-    transition-all duration-300 ease-in-out mt-2 border-2" 
-    :class="mode ? 'bg-red-400 text-textColor hover:bg-red-200 border-focusColor' 
-    : 'bg-bgColor text-textColor hover:bg-red-400 border-focusColor'">My Snake</button>
+    
 </template>
 
 <script lang="ts" setup>
