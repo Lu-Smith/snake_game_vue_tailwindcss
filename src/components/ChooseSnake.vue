2e245af77ffcb99f0be22e76cc1064bf703fbe71
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-01-18 18:19:56 +0000

    Updated ChooseSnake

--- src/components/ChooseSnake.vue
+++ src/components/ChooseSnake.vue
@@ -1,3 +1,14 @@
 <template>
-    choose your snake
+    <div class="w-full h-full mt-4 flex justify-center items-center">
+        <canvas
+        :class="mode ? 'bg-red-100' : 'bg-slate-800'"
+        class="border-2 border-focusColor"
+        ref="gameCanvas"
+        ></canvas>
+    </div>
+    <button>My Snake</button>
 </template>
+
+<script lang="ts" setup>
+    defineProps(['mode']);
+</script>
