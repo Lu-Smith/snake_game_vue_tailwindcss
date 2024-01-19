cec631d1ad4ac00eeb963a833dd16525dfb5abf6
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-01-19 17:14:28 +0000

    Updated logic in ChooseSnake.vue

--- src/components/ChooseSnake.vue
+++ src/components/ChooseSnake.vue
@@ -38,6 +38,8 @@
   import { ref, onMounted } from 'vue';
   import { drawSnakeHead2 } from '../utils/drawSnakeHead2Utils';
   import { drawBodyPart2 } from '../utils/drawSnakeBody2Utils';
+  import { drawSnakeHead } from '../utils/drawSnakeHeadUtils';
+  import { drawBodyPart } from '../utils/drawSnakeBodyUtils';
 
   defineProps(['mode']);
 
@@ -58,18 +60,34 @@
     const context = snakeCanvas.value?.getContext('2d');
 
     if (context) {
-      drawSnakeHead2(context, snake.value[0][0], snake.value[0][1], 10.5);
+        if(snakeNumber.value === 1) {
+            drawSnakeHead2(context, snake.value[0][0], snake.value[0][1], 10.5);
 
-      for (let i = 1; i < snake.value.length; i++) {
-        const bodyPart = snake.value[i];
+            for (let i = 1; i < snake.value.length; i++) {
+            const bodyPart = snake.value[i];
 
-        // Draw the body part
-        const distanceFactor = 12;
-        bodyPart[0] = snake.value[i - 1][0] + direction.value[0] * distanceFactor;
-        bodyPart[1] = snake.value[i - 1][1] + direction.value[1] * distanceFactor;
+            // Draw the body part
+            const distanceFactor = 12;
+            bodyPart[0] = snake.value[i - 1][0] + direction.value[0] * distanceFactor;
+            bodyPart[1] = snake.value[i - 1][1] + direction.value[1] * distanceFactor;
 
-        drawBodyPart2(context, bodyPart[0], bodyPart[1], i % 2 === 0);
-      }
+            drawBodyPart2(context, bodyPart[0], bodyPart[1], i % 2 === 0);
+            }
+        } else if(snakeNumber.value === 2) {
+            drawSnakeHead(context, snake.value[0][0], snake.value[0][1], 10.5, direction.value);
+
+            for (let i = 1; i < snake.value.length; i++) {
+            const bodyPart = snake.value[i];
+
+            // Draw the body part
+            const distanceFactor = 12;
+            bodyPart[0] = snake.value[i - 1][0] + direction.value[0] * distanceFactor;
+            bodyPart[1] = snake.value[i - 1][1] + direction.value[1] * distanceFactor;
+
+            drawBodyPart(context, bodyPart[0], bodyPart[1], i % 2 === 0);
+            }
+        }
+
     }
   });
 </script>
