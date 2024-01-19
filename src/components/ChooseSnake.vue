3786bdab38e8829eca5255dacf875dd68f50f7d7
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-01-19 18:44:17 +0000

    Updated logic for ChooseSnake.vue

--- src/components/ChooseSnake.vue
+++ src/components/ChooseSnake.vue
@@ -1,40 +1,37 @@
 <template>
-    <div class="flex justify-center items-center flex-col gap-4">
-        <div class="w-full h-full mt-4 flex justify-center items-center">
-            <canvas
-            v-if="snakeNumber === 1"
-            :class="mode ? 'bg-red-100' : 'bg-slate-800'"
-            class="border-2 border-focusColor"
-            ref="snakeCanvas"
-            ></canvas>
-            <canvas
-            v-if="snakeNumber === 2"
-            :class="mode ? 'bg-red-100' : 'bg-slate-800'"
-            class="border-2 border-focusColor"
-            ref="snakeCanvas"
-            ></canvas>
-        </div>
-        <div>
-            <button
-            @click="snakeChoice"
-            class="px-4 py-2 font-semibold rounded-lg shadow-sm text-sm md:text-base 
-            transition-all duration-300 ease-in-out border-2 mr-4" 
-            :class="mode ? 'bg-red-400 text-textColor hover:bg-red-200 border-focusColor' 
-            : 'bg-bgColor text-textColor hover:bg-red-400 border-focusColor'">
-            My Snake</button>        
-            <button
-            @click="nextSnake"
-            class="px-4 py-2 font-semibold rounded-lg shadow-sm text-sm md:text-base 
-            transition-all duration-300 ease-in-out border-2" 
-            :class="mode ? 'bg-red-400 text-textColor hover:bg-red-200 border-focusColor' 
-            : 'bg-bgColor text-textColor hover:bg-red-400 border-focusColor'">
-            Next</button>
-        </div>   
-    </div>  
+  <div class="flex justify-center items-center flex-col gap-4">
+    <div class="w-full h-full mt-4 flex justify-center items-center">
+      <canvas
+        :class="mode ? 'bg-red-100' : 'bg-slate-800'"
+        class="border-2 border-focusColor"
+        ref="snakeCanvas"
+      ></canvas>
+    </div>
+    <div>
+      <button
+        @click="snakeChoice"
+        class="px-4 py-2 font-semibold rounded-lg shadow-sm text-sm md:text-base 
+            transition-all duration-300 ease-in-out border-2 mr-4"
+        :class="mode ? 'bg-red-400 text-textColor hover:bg-red-200 border-focusColor' 
+            : 'bg-bgColor text-textColor hover:bg-red-400 border-focusColor'"
+      >
+        My Snake
+      </button>
+      <button
+        @click="nextSnake"
+        class="px-4 py-2 font-semibold rounded-lg shadow-sm text-sm md:text-base 
+            transition-all duration-300 ease-in-out border-2"
+        :class="mode ? 'bg-red-400 text-textColor hover:bg-red-200 border-focusColor' 
+            : 'bg-bgColor text-textColor hover:bg-red-400 border-focusColor'"
+      >
+        Next
+      </button>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import { ref, onMounted, watchEffect  } from 'vue';
+import { ref, onMounted, watchEffect } from 'vue';
 import { drawSnakeHead2 } from '../utils/drawSnakeHead2Utils';
 import { drawBodyPart2 } from '../utils/drawSnakeBody2Utils';
 import { drawSnakeHead1 } from '../utils/drawSnakeHead1Utils.ts';
@@ -61,62 +58,24 @@ const nextSnake = () => {
   drawSnake();
 };
 
+const drawSnakeHeadFunctions = [drawSnakeHead1, drawSnakeHead2, drawSnakeHead3, drawSnakeHead4];
+const drawBodyPartFunctions = [drawBodyPart1, drawBodyPart2, drawBodyPart3, drawBodyPart4];
+
 const drawSnake = () => {
   const context = snakeCanvas.value?.getContext('2d');
 
-  if (context) {
-    if (snakeNumber.value === 1) {
-      drawSnakeHead2(context, snake.value[0][0], snake.value[0][1], 10.5);
+  if (context && snakeNumber.value >= 1 && snakeNumber.value <= 4) {
+    drawSnakeHeadFunctions[snakeNumber.value - 1](context, snake.value[0][0], snake.value[0][1], 10.5);
 
-      for (let i = 1; i < snake.value.length; i++) {
-        const bodyPart = snake.value[i];
+    for (let i = 1; i < snake.value.length; i++) {
+      const bodyPart = snake.value[i];
 
-        // Draw the body part
-        const distanceFactor = 12;
-        bodyPart[0] = snake.value[i - 1][0] + direction.value[0] * distanceFactor;
-        bodyPart[1] = snake.value[i - 1][1] + direction.value[1] * distanceFactor;
+      // Draw the body part
+      const distanceFactor = 12;
+      bodyPart[0] = snake.value[i - 1][0] + direction.value[0] * distanceFactor;
+      bodyPart[1] = snake.value[i - 1][1] + direction.value[1] * distanceFactor;
 
-        drawBodyPart2(context, bodyPart[0], bodyPart[1], i % 2 === 0);
-      }
-    } else if (snakeNumber.value === 2) {
-      drawSnakeHead1(context, snake.value[0][0], snake.value[0][1], 10.5);
-
-      for (let i = 1; i < snake.value.length; i++) {
-        const bodyPart = snake.value[i];
-
-        // Draw the body part
-        const distanceFactor = 12;
-        bodyPart[0] = snake.value[i - 1][0] + direction.value[0] * distanceFactor;
-        bodyPart[1] = snake.value[i - 1][1] + direction.value[1] * distanceFactor;
-
-        drawBodyPart1(context, bodyPart[0], bodyPart[1], i % 2 === 0);
-      }
-    } else if (snakeNumber.value === 3) {
-      drawSnakeHead3(context, snake.value[0][0], snake.value[0][1], 10.5);
-
-      for (let i = 1; i < snake.value.length; i++) {
-        const bodyPart = snake.value[i];
-
-        // Draw the body part
-        const distanceFactor = 12;
-        bodyPart[0] = snake.value[i - 1][0] + direction.value[0] * distanceFactor;
-        bodyPart[1] = snake.value[i - 1][1] + direction.value[1] * distanceFactor;
-
-        drawBodyPart3(context, bodyPart[0], bodyPart[1], i % 2 === 0);
-      }
-    } else if (snakeNumber.value === 4) {
-      drawSnakeHead4(context, snake.value[0][0], snake.value[0][1], 10.5);
-
-      for (let i = 1; i < snake.value.length; i++) {
-        const bodyPart = snake.value[i];
-
-        // Draw the body part
-        const distanceFactor = 12;
-        bodyPart[0] = snake.value[i - 1][0] + direction.value[0] * distanceFactor;
-        bodyPart[1] = snake.value[i - 1][1] + direction.value[1] * distanceFactor;
-
-        drawBodyPart4(context, bodyPart[0], bodyPart[1], i % 2 === 0);
-      }
+      drawBodyPartFunctions[snakeNumber.value - 1](context, bodyPart[0], bodyPart[1], i % 2 === 0);
     }
   }
 };
