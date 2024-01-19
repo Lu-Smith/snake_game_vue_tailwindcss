baabd052a0dc83c23538f60d061dbbcaf11be180
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-01-19 18:20:24 +0000

    Updated ChooseSnake.vue

--- src/components/ChooseSnake.vue
+++ src/components/ChooseSnake.vue
@@ -29,65 +29,76 @@
             :class="mode ? 'bg-red-400 text-textColor hover:bg-red-200 border-focusColor' 
             : 'bg-bgColor text-textColor hover:bg-red-400 border-focusColor'">
             Next</button>
-        </div>
-       
+        </div>   
     </div>  
 </template>
 
 <script lang="ts" setup>
-  import { ref, onMounted } from 'vue';
-  import { drawSnakeHead2 } from '../utils/drawSnakeHead2Utils';
-  import { drawBodyPart2 } from '../utils/drawSnakeBody2Utils';
-  import { drawSnakeHead1 } from '../utils/drawSnakeHead1Utils';
-  import { drawBodyPart1 } from '../utils/drawSnakeBody1Utils';
+import { ref, onMounted, watchEffect  } from 'vue';
+import { drawSnakeHead2 } from '../utils/drawSnakeHead2Utils';
+import { drawBodyPart2 } from '../utils/drawSnakeBody2Utils';
+import { drawSnakeHead1 } from '../utils/drawSnakeHead1Utils.ts';
+import { drawBodyPart1 } from '../utils/drawSnakeBody1Utils';
 
-  defineProps(['mode']);
+defineProps(['mode']);
 
-  const snakeNumber = ref(1);
-  const snakeCanvas = ref<HTMLCanvasElement | null>(null);
-  const snake = ref([[62, 75], [80, 56], [80, 37], [80, 18], [80, 37], [80, 18], [80, 56], [80, 37], [80, 18], [80, 37], [80, 18], [80, 56], [80, 37], [80, 18], [80, 37], [80, 18]]);
-  const direction = ref([1, 0]);
+const snakeNumber = ref(1);
+const snakeCanvas = ref<HTMLCanvasElement | null>(null);
+const snake = ref([[62, 75], [80, 56], [80, 37], [80, 18], [80, 37], [80, 18], [80, 56], [80, 37], [80, 18], [80, 37], [80, 18], [80, 56], [80, 37], [80, 18], [80, 37], [80, 18]]);
+const direction = ref([1, 0]);
 
-  const snakeChoice = () => {
+const snakeChoice = () => {
+  // Implement snakeChoice logic if needed
+};
 
-  }
+const nextSnake = () => {
+  snakeNumber.value++;
+  console.log(snakeNumber.value);
+  drawSnake();
+};
 
-  const nextSnake = () => {
-    snakeNumber.value++;
-  }
+const drawSnake = () => {
+  const context = snakeCanvas.value?.getContext('2d');
 
-  onMounted(() => {
-    const context = snakeCanvas.value?.getContext('2d');
+  if (context) {
+    if (snakeNumber.value === 2) {
+      drawSnakeHead2(context, snake.value[0][0], snake.value[0][1], 10.5);
 
-    if (context) {
-        if(snakeNumber.value === 1) {
-            drawSnakeHead2(context, snake.value[0][0], snake.value[0][1], 10.5);
+      for (let i = 1; i < snake.value.length; i++) {
+        const bodyPart = snake.value[i];
 
-            for (let i = 1; i < snake.value.length; i++) {
-            const bodyPart = snake.value[i];
+        // Draw the body part
+        const distanceFactor = 12;
+        bodyPart[0] = snake.value[i - 1][0] + direction.value[0] * distanceFactor;
+        bodyPart[1] = snake.value[i - 1][1] + direction.value[1] * distanceFactor;
 
-            // Draw the body part
-            const distanceFactor = 12;
-            bodyPart[0] = snake.value[i - 1][0] + direction.value[0] * distanceFactor;
-            bodyPart[1] = snake.value[i - 1][1] + direction.value[1] * distanceFactor;
+        drawBodyPart2(context, bodyPart[0], bodyPart[1], i % 2 === 0);
+      }
+    } 
+    
+    if (snakeNumber.value === 1) {
+      drawSnakeHead1(context, snake.value[0][0], snake.value[0][1], 10.5);
 
-            drawBodyPart2(context, bodyPart[0], bodyPart[1], i % 2 === 0);
-            }
-        } else if(snakeNumber.value === 2) {
-            drawSnakeHead1(context, snake.value[0][0], snake.value[0][1], 10.5);
+      for (let i = 1; i < snake.value.length; i++) {
+        const bodyPart = snake.value[i];
 
-            for (let i = 1; i < snake.value.length; i++) {
-            const bodyPart = snake.value[i];
+        // Draw the body part
+        const distanceFactor = 12;
+        bodyPart[0] = snake.value[i - 1][0] + direction.value[0] * distanceFactor;
+        bodyPart[1] = snake.value[i - 1][1] + direction.value[1] * distanceFactor;
 
-            // Draw the body part
-            const distanceFactor = 12;
-            bodyPart[0] = snake.value[i - 1][0] + direction.value[0] * distanceFactor;
-            bodyPart[1] = snake.value[i - 1][1] + direction.value[1] * distanceFactor;
-
-            drawBodyPart1(context, bodyPart[0], bodyPart[1], i % 2 === 0);
-            }
-        }
-
+        drawBodyPart1(context, bodyPart[0], bodyPart[1], i % 2 === 0);
+      }
     }
-  });
+  }
+};
+
+watchEffect(() => {
+  drawSnake();
+});
+
+onMounted(() => {
+  drawSnake();
+});
+
 </script>
