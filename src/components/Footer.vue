a47907248598dde08eed70208de7cb57969fa075
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-01-22 17:12:16 +0000

    Updated Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,18 +6,17 @@
         <a 
         href="https://www.lunasmithart.com/"
         target="_blank"
-        :class="mode ? 'border-focusColor' : 'border-focusColor'"
-        class="text-focusColor border-b-2
+        :class="mode ? 'border-focusColor hover:bg-textColor text-addColor' : 'border-focusColor hover:bg-addColor text-focusColor'"
+        class=" border-b-2
         hover:border-none  transition-all duration-300 ease-in-out 
         hover:bg-addColor underline-offset-4 hover:underline-offset-8 rounded hover:p-2">
         Luna Smith</a> -
         <a 
         href="https://github.com/Lu-Smith/snake_game_vue_tailwindcss" 
         target="_blank"
-        :class="mode ? 'border-focusColor' : 'border-focusColor'"
-        class="text-focusColor border-b-2 
-        hover:border-none  transition-all duration-300 ease-in-out 
-        hover:bg-addColor underline-offset-4 hover:underline-offset-8 rounded hover:p-2">
+        :class="mode ? 'border-focusColor hover:bg-textColor text-addColor' : 'border-focusColor hover:bg-addColor text-focusColor'"
+        class="border-b-2 hover:border-none  transition-all duration-300 ease-in-out 
+         underline-offset-4 hover:underline-offset-8 rounded hover:p-2">
         GitHub</a>.
     </h3>
 </template>
