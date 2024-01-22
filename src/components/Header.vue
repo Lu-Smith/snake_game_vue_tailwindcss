b5bd7c902219c549ba3e970db676ad58fdcd9479
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-01-22 17:16:58 +0000

    Updated Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,10 @@
     class="text-center"
     :class="mode ? 'text-slate-800' : 'text-textColor'">
         <h1 class="text-xl md:text-2xl font-semibold mb-4">Welcome to 
-            <span class="font-bold" :class="mode ? 'text-addColor' : 'text-focusColor'">Snake Game</span>! </h1>
+            <span class="font-bold transition-all duration-300 ease-in-out" 
+            :class="mode ? 'text-addColor hover:text-textColor' : 'text-focusColor hover:text-addColor'">
+            Snake Game</span>! 
+        </h1>
         <p class="text-xs sm:text-sm md:text-base">Enjoy the classic thrill of maneuvering a snake through the digital realm. 
             Have fun and challenge yourself to grow as long as possible. Best of luck!</p>
     </div>
