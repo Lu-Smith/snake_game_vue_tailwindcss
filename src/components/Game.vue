a2acf3ae5b22f061105820f4257791c019c60b03
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-01-24 22:17:04 +0000

    Updated Game.vue

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -225,14 +225,14 @@
         newSnake.unshift(newSnakeHead);
 
         const head = [
-            snake.value[0][0] + direction.value[0] - 5,
+            snake.value[0][0] + direction.value[0] - 7,
             snake.value[0][1] + direction.value[1] - 5,
         ];
 
         const distanceX = Math.abs(head[0] - apple.value[0]);
         const distanceY = Math.abs(head[1] - apple.value[1]);
 
-        if (distanceX <= 5 && distanceY <= 5) {
+        if (distanceX <= 6 && distanceY <= 5) {
             score.value++;
             snake.value.push([0, 0]);
             placeNewApple();
